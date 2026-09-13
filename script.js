0e42190262fec3c42828dc55d1d9d0c02717b9ee
Simran-kumari1 <simrankumari2172008@gmail.com>
2026-09-13 19:12:35 +0530

    Add event listener to fetch random quote on button click

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function getQuote() {
+let button = document.getElementById("btn");
+
+button.addEventListener("click", function() {
+
     fetch("https://dummyjson.com/quotes/random")
         .then(response => response.json())
         .then(data => {
-            document.getElementById("quote").innerText =
-                '"' + data.quote + '"';
-
-            document.getElementById("author").innerText =
-                "- " + data.author;
+            document.getElementById("quote").innerText = data.quote;
+            document.getElementById("author").innerText = "- " + data.author;
         })
         .catch(error => {
-            console.log(error);
+            document.getElementById("quote").innerText =
+                "Something went wrong. Try again.";
         });
-}
+
+});
